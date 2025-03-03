8f98584df982cfe053b1bf9aab40b387eebdb56d
hawyeet <53439203+knz04@users.noreply.github.com>
2025-03-03 17:25:37 +0700

    5. Merged and unmerged Semantics trees

--- app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -8,6 +8,8 @@ import org.junit.Rule
 import org.junit.Test
 import androidx.compose.ui.test.onRoot
 import androidx.compose.ui.test.printToLog
+import androidx.compose.ui.test.hasParent
+import androidx.compose.ui.test.hasText
 
 class TopAppBarTest {
 
@@ -26,7 +28,13 @@ class TopAppBarTest {
         }
 
         composeTestRule
-            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Accounts.name)
+                        ),
+                useUnmergedTree = true
+            )
             .assertExists()
     }
 }
