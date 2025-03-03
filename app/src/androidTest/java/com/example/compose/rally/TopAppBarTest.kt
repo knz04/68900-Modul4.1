35269f19dc87d6189037012d34b2d21517766d6c
hawyeet <53439203+knz04@users.noreply.github.com>
2025-03-03 17:25:05 +0700

    4. Debugging tests

--- app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -1,10 +1,13 @@
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
 import com.example.compose.rally.RallyScreen
 import com.example.compose.rally.ui.components.RallyTopAppBar
 import org.junit.Rule
 import org.junit.Test
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.printToLog
 
 class TopAppBarTest {
 
@@ -12,7 +15,7 @@ class TopAppBarTest {
     val composeTestRule = createComposeRule()
 
     @Test
-    fun rallyTopAppBarTest_currentTabSelected() {
+    fun rallyTopAppBarTest_currentLabelExists() {
         val allScreens = RallyScreen.values().toList()
         composeTestRule.setContent {
             RallyTopAppBar(
@@ -24,6 +27,6 @@ class TopAppBarTest {
 
         composeTestRule
             .onNodeWithContentDescription(RallyScreen.Accounts.name)
-            .assertIsSelected()
+            .assertExists()
     }
 }
